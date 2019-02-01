a506b827b13f2588e253de1bd5dae37caf0a9571
gabel0287 <gabel0287@gmail.com>
2019-02-01 11:21:06 -0800

    try this

--- terraform_validate_no_variables.sh
+++ terraform_validate_no_variables.sh
@@ -11,7 +11,7 @@ for file_with_path in "$@"; do
   let "index+=1"
 done
 
-echo "${paths[*]}" | tr ' ' '\n' | sort -u
+echo "${paths[*]}" | tr ' ' '\n' | sort | uniq
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
